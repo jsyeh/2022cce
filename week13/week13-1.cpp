8377bc4070308df15f1c845b19802f045958cc9f
unknown <jsyeh@mail.mcu.edu.tw>
2022-11-29 12:03:46 +0800

    week13

---
+++ week13/week13-1.cpp
@@ -0,0 +1,16 @@
+///Fibonacii 費氏數列
+#include <stdio.h>
+int a[50];
+
+int main()
+{
+    a[0] = 0;
+    a[1] = 1;
+    printf("1 "); ///補印一下 a[1] 項
+
+    for(int i=2; i<20; i++){ ///從a[2]開始印
+        a[i] = a[i-1] + a[i-2];
+        printf("%d ", a[i] );
+    }
+
+}
