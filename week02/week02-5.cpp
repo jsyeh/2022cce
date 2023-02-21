ee147b1a10843b3588e89305717aad9a90b9cf6c
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-02-21 11:54:50 +0800

    今天是加入 week02

---
+++ week02/week02-5.cpp
@@ -0,0 +1,9 @@
+///Week02-5.cpp 了解 '\0' 和 0 的差別
+#include <stdio.h>
+
+int main()
+{
+    printf("%d\n", '\0');
+    printf("%d\n",   0 );
+    if( '\0' == 0 ) printf("竟然相同\n");
+}
---
+++ week02/week02-6.cpp
@@ -0,0 +1,14 @@
+///Week02-6.cpp step03-3.cpp 下週再講,今天不用寫
+#include <stdio.h>
+
+int main()
+{
+    int n=0;
+    for(int i=0; i<10; i++){
+        for(int j=0; j<10; j++){
+            printf("%d ", n);
+            n++;
+        }
+        printf("\n");
+    }
+}
